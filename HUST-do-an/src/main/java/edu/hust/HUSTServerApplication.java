4f6913694456c5ec37fdb16e75ba15b8eb08e435
thangnguyen <thangnh1997soict@gmail.com>
2021-04-13 23:33:37 +0700

    Add: swagger ui

--- HUST-do-an/src/main/java/edu/hust/HUSTServerApplication.java
+++ HUST-do-an/src/main/java/edu/hust/HUSTServerApplication.java
@@ -1,7 +1,13 @@
 package edu.hust;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -10,6 +16,16 @@ public class HUSTServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HUSTServerApplication.class, args);
+	}
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+		return new OpenAPI()
+				.info(new Info()
+								.title("sample application API")
+								.version(appVersion)
+								.description(appDesciption)
+								.termsOfService("http://swagger.io/terms/")
+								.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
